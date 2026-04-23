1ff8a1656e5ae30adcf32b2dfd51effe72d8306d
Chris Hanretty <chris.hanretty@gmail.com>
2026-04-23 15:25:42 +0100

    Wrote out MLE, god help me

---
+++ R/ewens_mle.R
@@ -0,0 +1,12 @@
+#' Maximum likelihood estimate of theta given sample vector with class memberships
+#'
+#' @param x A vector containing class memberships; sample size n and number of classes k are calculated from this
+#' 
+ewens_mle <- function(x) {
+    n <- length(x)
+    K <- length(unique(x))
+    score <- function(theta) K/theta - sum(1/(theta + seq(0, n-1)))
+    
+### Score is decreasing in theta, so bracket appropriately
+    uniroot(score, interval = c(1e-6, n))$root
+}
